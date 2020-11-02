35a77961657aaef932b3d158260b5b1f36924fb2
chairielazizi <azizihr2000@gmail.com>
2020-11-02 20:06:11 +0800

    extract the year from all the movies

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def main():
     soup = BeautifulSoup(html, 'html.parser')
     movietags = soup.select('td.titleColumn')
     movietag0 = movietags[0]
+    def get_year(movie_tag):
+        moviesplit = movie_tag.text.split()
+        year = moviesplit[-1]
+        print(moviesplit)
+        return year
+
+    years = [get_year(tag) for tag in movietags]
+
     print(movietag0)
+
+    
 
 if __name__ == '__main__':
     main()
