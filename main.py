ae738d7a25bf06a200b66416d2e8188f59380c23
chairielazizi <azizihr2000@gmail.com>
2020-11-02 20:10:24 +0800

    get the innermovietag <a>

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def main():
 
     soup = BeautifulSoup(html, 'html.parser')
     movietags = soup.select('td.titleColumn')
+    inner_movietags = soup.select('td.titleColumn a')
+
+
     movietag0 = movietags[0]
     def get_year(movie_tag):
         moviesplit = movie_tag.text.split()
         year = moviesplit[-1]
-        print(moviesplit)
+        # print(moviesplit)
         return year
 
     years = [get_year(tag) for tag in movietags]
 
-    print(movietag0)
+    innermovietag0 = inner_movietags[0]
+    print(innermovietag0)
+    # print(movietag0)
 
     
 
